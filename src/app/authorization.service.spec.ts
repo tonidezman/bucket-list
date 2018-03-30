f89b438bf3635c8f1a6ca91272bf3a653c871bb0
Toni Dezman <toni.dezman@gmail.com>
2018-03-30 09:33:50 +0200

    removes unnecessary inject from authorization service test

--- src/app/authorization.service.spec.ts
+++ src/app/authorization.service.spec.ts
@@ -1,4 +1,4 @@
-import { async, TestBed, inject } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthorizationService } from './authorization.service';
